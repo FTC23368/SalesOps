fa3f613904b60452306b2b31cb5cf0af35dd7018
FTC23368 <malihajburney@gmail.com>
2025-09-08 06:58:02 -0700

    revised code

--- smalltalk_agent.py
+++ smalltalk_agent.py
@@ -1,6 +1,6 @@
 from langchain_core.messages import BaseMessage
 from prompt_store import get_prompt
-from create_llm_message import create_llm_msg
+from create_llm_messages import create_llm_msg
 
 class SmallTalkAgent:
     def __init__(self, model):
@@ -11,10 +11,10 @@ class SmallTalkAgent:
         smalltalk_prompt = get_prompt("smalltalk").format(user_query=user_query)
         llm_messages = create_llm_msg(smalltalk_prompt, message_history)
         return self.model.stream(llm_messages)
-    
+
     def smalltalk_agent(self, state: dict) -> dict:
         return {
-            "lnode": "smalltalk agent",
+            "lnode": "smalltalk_agent",
             "incremental_response": self.generate_response(state['message_history']),
             "category": "smalltalk"
         }
